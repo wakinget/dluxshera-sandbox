74a563d31c3aa5d7c082601e581ed41a0e6ee67e
Dylan McKeithen <dylan.m.mckeithen@jpl.nasa.gov>
2026-01-13 14:31:16 -0800

    Add more instructive comments to working script.

--- work/scratch/refactored_astrometry_retrieval_eigen.py
+++ work/scratch/refactored_astrometry_retrieval_eigen.py
@@ -107,13 +107,26 @@ jax.config.update("jax_enable_x64", True)
 rng_seed = 42
 add_noise = False
 
-# Eigenmode Options
+# Eigenmode Settings
 use_eigen = True          # Enables re-parameterization
 whiten_basis = True       # If True, scales each eigenvector by 1/sqrt(lambda)
 truncate_k = None         # int or None; keep top-k eigenmodes when set
 truncate_by_eigval = None # float or None; only used when truncate_k is None.
                           # Drop eigenmodes with eigenvalue < this threshold.
 
+# Inference Settings
+# Only these keys will be optimized
+infer_keys = (
+    "binary.separation_as",
+    "binary.position_angle_deg",
+    "binary.x_position_as",
+    "binary.y_position_as",
+    "binary.log_flux_total",
+    "binary.contrast",
+    "system.plate_scale_as_per_pix",
+    "primary.zernike_coeffs_nm",
+    "secondary.zernike_coeffs_nm", # Optionally comment this one out
+)
 
 ##########################
 # Start Building the model
@@ -178,6 +191,7 @@ forward_truth_store = forward_truth_store.replace(
         "binary.x_position_as": 0.0,
         "binary.y_position_as": 0.0,
         "imaging.exposure_time_s": 1800.0,
+        # "system.plate_scale_as_per_pix": 0.355, # This is normally derived, but user may override here
     }
 )
 
@@ -214,22 +228,11 @@ data_var = data
 # Set up the inference
 ######################
 print("Configuring Inference...")
-# Choose inference keys
-# Only these keys will be optimized
-infer_keys = (
-    "binary.separation_as",
-    "binary.position_angle_deg",
-    "binary.x_position_as",
-    "binary.y_position_as",
-    "binary.log_flux_total",
-    "binary.contrast",
-    "system.plate_scale_as_per_pix",
-    "primary.zernike_coeffs_nm",
-    "secondary.zernike_coeffs_nm",
-)
+# Here we choose the subset of parameters that are listed in infer_keys
 inference_subspec = make_inference_subspec(base_spec=inference_spec, infer_keys=infer_keys, cfg=cfg)
 
 # Set up prior knowledge
+# Choose the type of distribution and the standard deviation for each parameter
 prior_info = {
     "binary.separation_as":          {"sigma": 1e-6, "dist": "Normal"},
     "binary.position_angle_deg":     {"sigma": 1e-3, "dist": "Uniform"},
@@ -247,26 +250,43 @@ prior_info = {
         "dist": "Normal",
     },
 }
+# PriorSpec.from_info() pulls the mean of the distribution from the provided store,
+# and uses the sigma and distribution type from prior_info
 prior_spec = PriorSpec.from_info(forward_truth_store, prior_info)
 
 print("Drawing starting point from priors...")
 # Draw an initial point for the model from the priors
 rng_key, split_key = jr.split(rng_key)
 init_store = prior_spec.sample_near(forward_truth_store, rng_key=split_key, keys=infer_keys)
-init_psf = binder.model(strip_structural(init_store))
+# We use prior_spec.sample_near to draw a random sample from the priors using the
+# provided forward_truth_store as the center (mean) of the distribution
+init_psf = binder.model(strip_structural(init_store, structural_keys=binder.structural_keys()))
+# To compute the initial (perturbed) PSF, we can provide a store to the binder.model() method.
+# By default, the .model() method will throw an error if the user provides any structural parameter updates.
+# We can use the strip_structural helper to remove structural keys listed by the binder
+
+# We can allow structural updates by passing the 'allow_rebuild' input argument to .model()
+# In this case, the binder will rebuild the entire model using the provided store, and then evaluate the PSF
+# This is slower due to the rebuild, and so we prefer to be strict about passing only non-structural parameters
+# init_psf = binder.model(init_store, allow_rebuild=True) # Should be identical to earlier call
 
 print("Building the loss function...")
-# Build the Loss function
+# NLL = Negative Log-Likelihood
+# make_binder_nll_fn builds and returns the loss function: nll_loss_fn(theta)
+# nll_loss_fn(theta) gives the negative log-likelihood loss for a given input theta vector
+# Internally, the input theta is merged into the binder, a model image is produced and compared to the data image,
+# and the negative log-likelihood is returned.
+# User may choose a "gaussian" or "poisson" log-likelihood, and also to return the "sum" or "mean" over the pixels
 nll_loss_fn, theta0 = make_binder_nll_fn(
     binder=binder,
     infer_keys=infer_keys,
     data=data,
     var=data_var,
-    noise_model="gaussian",
-    reduce="sum",
+    noise_model="gaussian", # "gaussian" or "poisson"
+    reduce="sum", # "sum" or "mean"
     theta0_store=init_store,
 )
-# nll_loss_fn(theta) gives the negative log-likelihood loss for a given input theta vector
+# Generate human-readable labels for parameter names
 fim_labels = generate_fim_labels_refactor(
     infer_keys,
     cfg=cfg,
@@ -278,7 +298,7 @@ def map_loss_fn(theta: np.ndarray) -> np.ndarray:
     nll_loss = nll_loss_fn(theta)
     prior_gaussian_loss = prior_spec.quadratic_penalty(store_theta, center_store=forward_truth_store, keys=infer_keys)
     return nll_loss + prior_gaussian_loss
-# the map_loss_fn(theta) gives the maximum a posteriori (MAP) loss value for an input theta vector
+# the map_loss_fn(theta) gives the Maximum A Posteriori (MAP) loss value for an input theta vector
 # This incorporates a quadratic penalty from prior knowledge on top of the NLL loss
 
 # Choose which loss function to use
@@ -442,7 +462,7 @@ else:
 final_store = store_unpack_params(inference_subspec, theta_final, init_store)
 
 # Collect GD outputs
-final_psf = binder.model(strip_structural(final_store))
+final_psf = binder.model(strip_structural(final_store, structural_keys=binder.structural_keys()))
 
 ##################
 # Print a Summary
