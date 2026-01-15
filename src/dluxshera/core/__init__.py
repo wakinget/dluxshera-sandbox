7118ed50d4a664fcf5e3cc3c92776d7daf0cc4a4
Dylan McKeithen <dylan.m.mckeithen@jpl.nasa.gov>
2026-01-15 09:42:57 -0800

    Delete thin re-exports kept originally for backwards compatibility.

--- src/dluxshera/core/__init__.py
+++
@@ -1,7 +0,0 @@
-# src/dluxshera/core/__init__.py
-
-from .universe import build_alpha_cen_source
-
-__all__ = [
-    "build_alpha_cen_source",
-]
--- src/dluxshera/core/universe.py
+++
@@ -1,9 +0,0 @@
-# src/dluxshera/core/universe.py
-
-from __future__ import annotations
-
-from ..builders.source import build_alpha_cen_source
-
-__all__ = [
-    "build_alpha_cen_source",
-]
